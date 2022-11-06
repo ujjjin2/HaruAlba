ab75eda4a36d0051528626536249a3c1210eeddd
Seungwan Han <hseungwan@naver.com>
2022-11-07 00:27:20 +0900

    JAVA DAO 추가

---
+++ java/user/User.java
@@ -0,0 +1,77 @@
+package user;
+
+public class User {
+	
+	private String userID;
+	private String userPASSWORD;
+	private String userNAME;
+	private String userLOCATION;
+	private int userAGE;
+	private int userGENDER;
+	private int userPHONE;
+	private String userNICKNAME;
+	private int userRATING;
+	private String storeLOCATION;
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPASSWORD() {
+		return userPASSWORD;
+	}
+	public void setUserPASSWORD(String userPASSWORD) {
+		this.userPASSWORD = userPASSWORD;
+	}
+	public String getUserNAME() {
+		return userNAME;
+	}
+	public void setUserNAME(String userNAME) {
+		this.userNAME = userNAME;
+	}
+	public String getUserLOCATION() {
+		return userLOCATION;
+	}
+	public void setUserLOCATION(String userLOCATION) {
+		this.userLOCATION = userLOCATION;
+	}
+	public int getUserAGE() {
+		return userAGE;
+	}
+	public void setUserAGE(int userAGE) {
+		this.userAGE = userAGE;
+	}
+	public int getUserGENDER() {
+		return userGENDER;
+	}
+	public void setUserGENDER(int userGENDER) {
+		this.userGENDER = userGENDER;
+	}
+	public int getUserPHONE() {
+		return userPHONE;
+	}
+	public void setUserPHONE(int userPHONE) {
+		this.userPHONE = userPHONE;
+	}
+	public String getUserNICKNAME() {
+		return userNICKNAME;
+	}
+	public void setUserNICKNAME(String userNICKNAME) {
+		this.userNICKNAME = userNICKNAME;
+	}
+	public int getUserRATING() {
+		return userRATING;
+	}
+	public void setUserRATING(int userRATING) {
+		this.userRATING = userRATING;
+	}
+	public String getStoreLOCATION() {
+		return storeLOCATION;
+	}
+	public void setStoreLOCATION(String storeLOCATION) {
+		this.storeLOCATION = storeLOCATION;
+	}
+
+}
---
+++ java/user/UserDAO.java
@@ -0,0 +1,44 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet; // 단축키 : ctrl + shift + 'o'
+
+public class UserDAO {
+	private Connection conn; //db 접근 객체 
+	private PreparedStatement pstmt;
+	private ResultSet rs; // db 결과를 담는 객체
+	
+	public UserDAO() { // dao 생성자에서 db connection 
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/haru?serverTimezone=UTC";
+			String dbID = "haru"; //계정
+			String dbPassword = "haru"; //비밀번호
+			Class.forName("com.mysql.cj.jdbc.Driver"); //mysql에 접속을 도와주는 라이브러리 
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	// 로그인 기능 
+	public int login(String userID, String userPASSWORD) {
+		String SQL = "SELECT userPASSWORD FROM USER WHERE userID = ?";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID); 
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				if (rs.getString(1).equals(userPASSWORD)) // rs.getString(1) : select된 첫번째 컬럼
+					return 1; //로그인 성공
+				else
+					return 0; // 비밀번호 틀림
+			}
+			return -1; // 아이디 없음 
+		}catch(Exception e) {
+			e.printStackTrace();
+			
+		}
+		return -2; //DB 오류 
+	}
+}
