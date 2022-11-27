f64bf5391d7231f13126e07c96da770b2b8531da
Seungwan Han <hseungwan@naver.com>
2022-11-27 16:17:40 +0900

    암호화 주석

--- java/util/SALT.java
+++ java/util/SALT.java
@@ -6,14 +6,14 @@ public class SALT {
 		
 		String result = "";
 		
-		      char ch1 = (char) ((Math.random() * 26) + 97);
-		      char ch2 = (char) ((Math.random() * 26) + 97);
+		      char ch1 = (char) ((Math.random() * 26) + 97); // 랜덤 알파벳 생성
+		      char ch2 = (char) ((Math.random() * 26) + 97); // 랜덤 알파벳 생성
 
 		
 		 result += ch1 ;
 		 result += ch2 ;
 		
-		return result;
+		return result; // 문자열 반환
 
 }
 
